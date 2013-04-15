6563627361ad183f5e3409ea61782f233f263b1e
RvcHeather <4myed12@gmail.com>
2013-04-14 23:36:19 -0500

    Revert "Revert "Week11MyTrailers""
    This reverts commit 35595d7bcc192a0010f0211fa1c20067bd76b9ab.

---
+++ trailer2ViewController.h
@@ -0,0 +1,16 @@
+//
+//  trailer2ViewController.h
+//  MyMovies
+//
+//  Created by iMac on 4/14/13.
+//  Copyright (c) 2013 Heather Pruitt. All rights reserved.
+//
+
+#import <MediaPlayer/MediaPlayer.h>
+#import <UIKit/UIKit.h>
+
+@interface trailer2ViewController : UIViewController
+{
+    MPMoviePlayerController *player;
+}
+@end
